a4f3066751f5c9c3ff646aaf21b05694257ad398
«Yana <yananikolayeva@icloud.com.com>
2021-03-12 19:06:48 +0300

    cart

--- main.js
+++ main.js
@@ -1,20 +1,76 @@
-const goods =[
-    {title:'Shirt', price:'150', img: "./img/download.png", button: "Добавить"},
-    {title:'Socks', price:'50', img:  "./img/download.png", button: "Добавить"},
-    {title:'Jacket', price:'350', img: "./img/download.png", button: "Добавить"},
-    {title:'Shoes', price:'250', img:  "./img/download.png", button: "Добавить"},
-];
+// Класс для товара с методом html- разметки
 
-const $goodsList = document.querySelector('.goods-list');
-const renderGoodsItem =({title, price, img, button}) => {
-    return `<div class= "goods-item"><img class="goods-img" src="${img}"><h3>${title}</h3><p>${price} &#36;</p><button type="submit">${button}</button></div>`;
-
+class GoodsItem {
+    constructor(title, price, img, button) {
+        this.title = title;
+        this.price = price;
+        this.img = img;
+        this.button = button;
+    }
+    renderHtml () {   
+    return `<div class= "goods-item"><img class="goods-img" 
+    src="${this.img}"><h3>${this.title}</h3><p>${this.price} &#36;</p><button type="submit">Добавить</button></div>`;
+    }
 }
 
-const renderGoodsList = (list=goods) => {
-    let goodsList = list.map(
-        item => renderGoodsItem(item)
-    ).join('\n');
-    $goodsList.insertAdjacentHTML('beforeend', goodsList);
+// Класс со списком товаров и методом для заполнения списка
+
+class GoodsList {
+    constructor () {
+        this.goods = [ ];
+    }
+    fetchGoods () {
+        this.goods = [
+            {title:'Shirt', price:'150', img: "./img/download.png"},
+            {title:'Socks', price:'50', img:  "./img/download.png"},
+            {title:'Jacket', price:'350', img: "./img/download.png"},
+            {title:'Shoes', price:'250', img:  "./img/download.png"},
+        ];
+    }
+    render () {
+        let listHtml = '';
+        this.goods.forEach (good => {
+            const goodItem = new GoodsItem(good.title, good.price, good.img);
+            listHtml+= goodItem.renderHtml();
+        });
+        document.querySelector('.goods-list').innerHTML = listHtml;
+    }
 }
-renderGoodsList();
+
+
+const list = new GoodsList();
+list.fetchGoods();
+list.render();
+
+
+// Класс для корзины
+const cartBtn = document.querySelector ('.basket');
+
+class Cart {
+    constructor() {
+        this.goods = [ ];
+    }
+    addCartGood (cartGood) {
+        this.goods.push(cartGood);
+    }
+    totalCartPrice () {
+        let totalPrice = document.querySelector('.summary');
+        let sum = 0;
+        this.goods.forEach (good => {
+            sum+= good.price;
+        });
+        totalPrice.insertAdjacentText = `Итого ${sum} &#36;`;
+    }
+    
+}
+
+var  renderCart = () => {
+    const cart = new Cart();
+    cart.addCartGood(list.goods[0]);
+    cart.addCartGood(list.goods[1]);
+    cart.addCartGood(list.goods[2]);
+    cart.totalCartPrice();
+    
+}
+
+cartBtn.addEventListener('click', renderCart);
